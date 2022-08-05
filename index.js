e65324fd629975654e339f2607adfc8c5966b8a2
UWAYONoella <noellawayo2019@gmail.com>
2022-08-05 12:34:24 +0200

    Feat:all

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const mtgo=document.getElementsByClassName("main");
-console.log(mtgo);
-const tgo=Array.from(mtgo);
-console.log(tgo);
+// console.log(mtgo);
+const subt=document.getElementsByClassName("sub");
+// console.log(subt);
+const tgo=Array.from(subt);
+// console.log(tgo);
 tgo.map((item)=>item.firstElementChild.addEventListener("click",e=>{
-    e.target.nextElementsibling.classList.toggle("top");
-    console.log(e.target.nextElementsibling);
+    e.target.nextElementSibling.classList.toggle("top");
+
+    // console.log(e.target.nextElementsibling);
 }))
 
-const body=document.querySelector("body");
-const right=document.createElement("right");
+const body=document.getElementsByTagName("body")[0];
+const right=document.createElement("div");
 right.classList.add("right");
 body.insertBefore(right,body.children[0]);
 console.log(body);
-const up=document.createElement("up");
+const up=document.createElement("p");
 up.classList.add("up");
 body.insertBefore(up,right);
 console.log(body);
 
-let butn=document.createElement("button");
+const butn=document.createElement("button");
 butn.classList.add("button");
 butn.innerText="click"
 body.insertBefore(butn,body.children[0]);
 
-let video=document.getElementById("video");
+const video=document.getElementById("video");
 
 butn.addEventListener("click",e=>{
-   e.right.classList.toggle("cover");
+   right.classList.toggle("righ");
     up.classList.toggle("down");
     
 })
 
-let song=document.getElementById("song");
+const song=document.getElementById("song");
 song.addEventListener("click",e=>{
     e.target.innerText="song";
     e.target.style.backgroundColor="yellow";
     e.target.style.color="red";
-    video.classList.toggle("showVideo");
+    video.classList.toggle("myVideo");
 })
