a1ad95a2f507e78324de6303d402f0b478de07f3
UWAYONoella <noellawayo2019@gmail.com>
2022-08-26 16:13:59 +0200

    Feat:toggle

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const mtgo=document.getElementsByClassName("main");
-// console.log(mtgo);
+console.log(mtgo);
 const subt=document.getElementsByClassName("sub");
 // console.log(subt);
 const tgo=Array.from(subt);
-// console.log(tgo);
+console.log(tgo);
 tgo.map((item)=>item.firstElementChild.addEventListener("click",e=>{
     e.target.nextElementSibling.classList.toggle("top");
 
@@ -22,7 +22,7 @@ console.log(body);
 
 const butn=document.createElement("button");
 butn.classList.add("button");
-butn.innerText="click"
+butn.innerText="click here to see well"
 body.insertBefore(butn,body.children[0]);
 
 const video=document.getElementById("video");
@@ -35,8 +35,8 @@ butn.addEventListener("click",e=>{
 
 const song=document.getElementById("song");
 song.addEventListener("click",e=>{
-    e.target.innerText="song";
-    e.target.style.backgroundColor="yellow";
-    e.target.style.color="red";
+    e.target.innerText="HAPPIEST BIRTHDAY BEST FRIEND YOU ARE SO IMPORTANT let's GOD continue to lead the steps of your legs";
+    e.target.style.backgroundColor="black";
+    e.target.style.color="white";
     video.classList.toggle("myVideo");
 })
